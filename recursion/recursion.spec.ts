5e26432b74130711e045286b144a673aa2590bc9
Sid Hayoun Lee <sidhlee@gmail.com>
2021-12-20 07:52:33 -0500

    Recursion: problem set

--- recursion/recursion.spec.ts
+++ recursion/recursion.spec.ts
@@ -1,4 +1,13 @@
-import { collectOddValues, collectOddValuesPure } from './recursion';
+import {
+  collectOddValues,
+  collectOddValuesPure,
+  factorial,
+  fib,
+  power,
+  productOfArray,
+  reverse,
+  sumRange,
+} from './recursion';
 
 describe('collectOddValues', () => {
   it('works', () => {
@@ -9,5 +18,63 @@ describe('collectOddValues', () => {
 describe('collectOddValuesPure', () => {
   it('works', () => {
     expect(collectOddValuesPure([1, 2, 3, 4, 5])).toEqual([1, 3, 5]);
+  });
+});
+
+describe('power', () => {
+  it('should return one for the exponent of zero', () => {
+    expect(power(2, 0)).toBe(1);
+  });
+  it('should work', () => {
+    expect(power(2, 2)).toBe(4);
+    expect(power(2, 4)).toBe(16);
+  });
+});
+
+describe('factorial', () => {
+  it('should return factorial', () => {
+    expect(factorial(1)).toBe(1);
+    expect(factorial(2)).toBe(2);
+    expect(factorial(4)).toBe(24);
+    expect(factorial(7)).toBe(5040);
+  });
+
+  it('should return 1 given 0', () => {
+    expect(factorial(0)).toBe(1);
+  });
+});
+
+describe('productOfArray', () => {
+  it('should work', () => {
+    expect(productOfArray([1, 2, 3])).toBe(6);
+    expect(productOfArray([1, 2, 3, 10])).toBe(60);
+  });
+  it('should throw when given an empty array', () => {
+    // must call inside another function
+    expect(() => productOfArray([])).toThrow(
+      'array must have at least one number'
+    );
+  });
+});
+
+describe('sumRange', () => {
+  it('should work', () => {
+    expect(sumRange(6)).toBe(21);
+    expect(sumRange(10)).toBe(55);
+  });
+});
+
+describe('fib', () => {
+  it('should work', () => {
+    expect(fib(4)).toBe(3);
+    expect(fib(10)).toBe(55);
+    expect(fib(28)).toBe(317811);
+    expect(fib(35)).toBe(9227465);
+  });
+});
+
+describe('reverse', () => {
+  it('should work', () => {
+    expect(reverse('abc')).toBe('cba');
   });
 });
--- recursion/recursion.ts
+++ recursion/recursion.ts
@@ -25,3 +25,54 @@ export function collectOddValuesPure(arr: number[]): number[] {
   }
   return newArr.concat(collectOddValuesPure(arr.slice(1)));
 }
+
+/**
+ * Should return the power of the base to the exponent. Assume that the base and the exponent is greater than or equal to 0.
+ * @param base
+ * @param exponent
+ */
+export function power(base: number, exponent: number): number {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+/**
+ * Returns the factorial of the given number. Factorial of 0 is always 1.
+ */
+export function factorial(num: number): number {
+  if (num < 0) throw Error('cannot take factorial of negative number.');
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+}
+
+export function productOfArray(numbers: number[]): number {
+  if (numbers.length === 0) throw Error('array must have at least one number');
+  if (numbers.length === 1) return numbers[0];
+  return numbers[0] * productOfArray(numbers.slice(1));
+}
+
+/**
+ * Adds up all the numbers from 0 to the given number.
+ * @param num positive integer
+ */
+export function sumRange(num: number): number {
+  if (num === 1) return 1;
+  return num + sumRange(num - 1);
+}
+
+/**
+ * Returns nth number of fibonacci sequence eg. 1, 1, 2, 3, 5,...
+ * @param num positive integer
+ */
+export function fib(num: number): number {
+  if (num < 3) return 1;
+  return fib(num - 1) + fib(num - 2);
+}
+
+// 1, 1, 2, 3, 5, 8, 11
+// 1  2  3  4  5  6  7
+
+export function reverse(str: string): string {
+  if (str.length === 1) return str;
+  return str[str.length - 1] + reverse(str.slice(0, -1));
+}
