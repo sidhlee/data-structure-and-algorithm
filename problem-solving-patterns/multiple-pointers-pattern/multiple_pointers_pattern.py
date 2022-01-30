cac46e8796903634ae65c278769ef82b63a63a3d
Sid Hayoun Lee <sidhlee@gmail.com>
2022-01-29 22:30:08 -0500

    multiple_pointers_pattern

--- problem-solving-patterns/multiple-pointers-pattern/multiple_pointers_pattern.py
+++ problem-solving-patterns/multiple-pointers-pattern/multiple_pointers_pattern.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import Any, List, Union
 
 
 def sum_zero(integers: List[int]) -> Union[List[int], None]:
@@ -13,3 +13,67 @@ def sum_zero(integers: List[int]) -> Union[List[int], None]:
         else:
             return [integers[i], integers[j]]
     return None
+
+
+def count_unique_values(sorted_ints: List[int]) -> int:
+    if len(sorted_ints) == 0:
+        return 0
+    # If there is at least one item in the list, it should count as a unique value
+    count = 1
+    i = 0
+    j = 1
+    while j < len(sorted_ints):
+        if sorted_ints[i] == sorted_ints[j]:
+            j += 1
+        # advance j until it points to a different integer
+        else:
+            count += 1
+            i = j  # reset i to that new integer to compare the future values
+            j += 1  # and advance j
+    return count
+
+
+# [1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]
+#                    i
+#                    j
+
+
+def are_there_duplicates(*args: Any):
+    sorted_args = list(args)
+    sorted_args.sort()
+    i = 1
+    while i < len(args):
+        if sorted_args[i - 1] == sorted_args[i]:
+            return True
+        else:
+            i += 1
+    return False
+
+
+def average_pair(sorted_ints: List[int], target_avg: float) -> bool:
+    if len(sorted_ints) < 2:
+        return False
+    i = 0
+    j = len(sorted_ints) - 1
+    while i < j:
+        avg = (sorted_ints[i] + sorted_ints[j]) / 2
+        if avg > target_avg:
+            j -= 1
+        elif avg < target_avg:
+            i += 1
+        else:
+            return True
+    return False
+
+
+def is_subsequence(str1: str, str2: str) -> bool:
+    index1 = 0
+    index2 = 0
+    while index1 < len(str1) and index2 < len(str2):
+        if str1[index1] == str2[index2]:
+            index1 += 1
+            index2 += 1
+        else:
+            index2 += 1
+    # we found all characters in str1 in str2
+    return index1 == len(str1)
--- problem-solving-patterns/multiple-pointers-pattern/test_multiple_pointers_pattern.py
+++ problem-solving-patterns/multiple-pointers-pattern/test_multiple_pointers_pattern.py
@@ -1,5 +1,11 @@
 import unittest
-from multiple_pointers_pattern import sum_zero
+from multiple_pointers_pattern import (
+    sum_zero,
+    count_unique_values,
+    are_there_duplicates,
+    average_pair,
+    is_subsequence,
+)
 
 
 class SumZeroTestCase(unittest.TestCase):
@@ -15,3 +21,54 @@ class SumZeroTestCase(unittest.TestCase):
 
     def test_two_zeros(self):
         self.assertEqual(sum_zero([0, 0]), [0, 0])
+
+
+class CountUniqueValues(unittest.TestCase):
+    def test_success(self):
+        self.assertEqual(count_unique_values([1, 1, 1, 1, 1, 2]), 2)
+        self.assertEqual(count_unique_values([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]), 7)
+        self.assertEqual(count_unique_values([-2, -1, -1, 0, 1]), 4)
+
+    def test_empty_list(self):
+        self.assertEqual(count_unique_values([]), 0)
+
+
+class AreThereDuplicatesTestCase(unittest.TestCase):
+    def test_number_duplicates(self):
+        self.assertTrue(are_there_duplicates(1, 2, 2))
+
+    def test_string_duplicates(self):
+        self.assertTrue(are_there_duplicates("a", "b", "c", "a"))
+
+    def test_no_duplicates(self):
+        self.assertFalse(are_there_duplicates(1, 2, 3))
+
+
+class AveragePairTestCase(unittest.TestCase):
+    def test_found_pair(self):
+        self.assertTrue(average_pair([1, 2, 3], 2.5))
+        self.assertTrue(average_pair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8))
+
+    def test_no_pair(self):
+        self.assertFalse(average_pair([-1, 0, 3, 4, 5, 6], 4.1))
+
+    def test_empty_list(self):
+        self.assertFalse(average_pair([], 4))
+
+    def test_one_integer(self):
+        self.assertFalse(average_pair([1], 1))
+
+    def test_two_same_integers(self):
+        self.assertTrue(average_pair([1, 1], 1))
+
+
+class IsSubsequenceTestCase(unittest.TestCase):
+    def test_str1_in_str2(self):
+        self.assertTrue(is_subsequence("hello", "hello world"))
+
+    def test_inserted_characters(self):
+        self.assertTrue(is_subsequence("sing", "sting"))
+        self.assertTrue(is_subsequence("abc", "abracadabra"))
+
+    def test_character_order_changed(self):
+        self.assertFalse(is_subsequence("abc", "acb"))
