941f6ef7c95a58d2671ebd9a04f24e441dd1f42f
Sid Hayoun Lee <sidhlee@gmail.com>
2022-02-12 02:23:07 -0500

    seaching_algorithm finish

--- searching-algorithms/searching_algorithms.py
+++ searching-algorithms/searching_algorithms.py
@@ -1,3 +1,4 @@
+import math
 from typing import Any, List, TypeVar
 from abc import ABCMeta, abstractmethod
 
@@ -12,13 +13,36 @@ class Comparable(metaclass=ABCMeta):
 CT = TypeVar("CT", bound=Comparable)
 
 
-def linear_search(arr: List[Any], item: Any) -> int:
-    pass
+def linear_search(arr: List[Any], target: Any) -> int:
+    for i, item in enumerate(arr):
+        if item == target:
+            return i
+    return -1
 
 
-def binary_search(arr: List[CT], item: CT) -> int:
-    pass
+def binary_search(arr: List[CT], target: CT) -> int:
+    head = 0
+    tail = len(arr) - 1
+    while head <= tail:
+        mid = math.floor(tail - head / 2)
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            head = mid + 1
+        elif target < arr[mid]:
+            tail = mid - 1
+    return -1
 
 
 def naive_string_search(source: str, sub_str: str) -> int:
-    pass
+    count = 0
+
+    for i, char in enumerate(source):
+        if char == sub_str[0]:
+            match = True
+            for j, sub_char in enumerate(sub_str):
+                if sub_char != source[i + j]:
+                    match = False
+            if match:
+                count += 1
+    return count
