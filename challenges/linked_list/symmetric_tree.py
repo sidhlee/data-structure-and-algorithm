d0330f054807228b47a0c833d96b497764e90005
sidhlee <sidhlee@gmail.com>
2022-07-13 07:17:58 -0400

    symmetric_tree

--- challenges/linked_list/symmetric_tree.py
+++ challenges/linked_list/symmetric_tree.py
@@ -68,3 +68,38 @@ class Solution:
                 return False
             # set the checked nodes as the new queue
             queue = deque(nodes)
+
+    def isSymmetric_recursion_optimized(self, root: Optional[TreeNode]) -> bool:
+        """
+        2022-07-13 07:07:41
+        Runtime: 39 ms (85%)
+        Memory Usage: 13.8 MB (94%)
+
+        Using recursion to traverse to the outermost leaves then backup to traverse to the inner nodes.
+
+        base case:
+        - both children are None
+
+        not symmetric if:
+        - only one of the nodes is None
+        - if node values are not equal
+
+        optimization:
+        This algorithm checks the entire tree and start again with the first left and right child switched.
+        will return True if the passed nodes are right and left child of the root.
+        """
+
+        def is_sym(node1, node2):
+            if node1 is root.right and node2 is root.left:
+                return True
+            self.visited_root = True
+            if node1 is None and node2 is None:
+                return True
+            if (node1 is None) != (node2 is None):
+                return False
+            if node1.val != node2.val:
+                return False
+
+            return is_sym(node1.left, node2.right) and is_sym(node1.right, node2.left)
+
+        return is_sym(root, root)
