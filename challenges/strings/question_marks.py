6348be86661da2bfd097e93506464c2ece02428f
Sid Hayoun Lee <sidhlee@gmail.com>
2023-01-20 06:51:50 -0500

    question_marks_simpler

--- challenges/strings/question_marks.py
+++ challenges/strings/question_marks.py
@@ -1,4 +1,7 @@
 def QuestionsMarks_linear(strParam: str):
+    """
+    2023-01-20 06:46:41
+    """
     pair_sum = 0
     question_marks = 0
     has_pair = False
@@ -20,4 +23,26 @@ def QuestionsMarks_linear(strParam: str):
     return "true" if has_pair else "false"
 
 
-QuestionMarks = QuestionsMarks_linear
+def question_marks_simpler(string: str):
+    """
+    2023-01-20 06:46:48
+    """
+    num = 11  # no single number can be added to this and make 10
+    question_marks = 0
+    has_pair = False
+    for char in string:
+        if char.isnumeric():
+            if num + int(char) == 10:
+                if question_marks != 3:
+                    return "false"
+                has_pair = True
+            # always update the number because we only care about single numbers
+            num = int(char)
+            # once we see a number, reset the question marks back to 0
+            question_marks = 0
+        elif char == "?":
+            question_marks += 1
+    return "true" if has_pair else "false"
+
+
+QuestionMarks = question_marks_simpler
