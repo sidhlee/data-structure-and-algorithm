7150bab5e05fb89f011b14aae4b3b4793a20064f
Sid Hayoun Lee <sidhlee@gmail.com>
2023-01-29 12:06:05 -0500

    BracketCombinations_binary_decision_tree

--- challenges/math/bracket_combinations.py
+++ challenges/math/bracket_combinations.py
@@ -3,15 +3,19 @@ def BracketCombinations_binary_decision_tree(num):
     2023-01-24 06:52:20
     """
 
-    def inner(num_open, num_closed):
+    def inner(num_open=num, num_closed=num):
+        # Ran out of either of brackets -> 1 decision from here
         if num_open <= 0 or num_closed <= 0:
             return 1
-        if num_open >= num_closed:
+        # If we have more remaining open bracket, we can branch out into 2 cases
+        if num_open > num_closed:
             return inner(num_open - 1, num_closed) + inner(num_open, num_closed - 1)
+        # If we have the same number of remaining brackets,
+        # we can only branch into using closed bracket
         else:
             return inner(num_open, num_closed - 1)
 
-    return inner(num - 1, num - 1)
+    return inner()
 
 
 def bracket_combinations_math(num):
@@ -24,11 +28,13 @@ def bracket_combinations_math(num):
     - must start with 0 and end with 1
     - at any given index, there cannot be more 1s than 0s.
 
-    [0] 1 - only 0
-    [1] 2 - 0 | 1
-    [2]
+    Permutation on all elements = (2 * num)! -> 6!
+    2 sets of 3 interchangeable numbers = num! * num! -> 3! * 3!
+
+    1/2 of permutation is invalid (starting with 1)
+
     """
     return None
 
 
-bracket_combinations = bracket_combinations_math
+bracket_combinations = BracketCombinations_binary_decision_tree
