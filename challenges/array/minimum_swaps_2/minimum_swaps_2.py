27b12f9cc0bc0d74226420c7517a55372de0218c
Sid Hayoun Lee <sidhlee@gmail.com>
2023-03-01 08:14:24 -0500

    minimum_swaps failing not using fixed index

--- challenges/array/minimum_swaps_2/minimum_swaps_2.py
+++ challenges/array/minimum_swaps_2/minimum_swaps_2.py
@@ -26,4 +26,51 @@ def minimumSwaps_send_to_correct_position(arr):
     return swaps
 
 
+def minimumSwaps(arr):
+    count = 0
+    i = 0
+    while i < len(arr) - 1:
+        o_idx = arr[i] - 1
+        if o_idx == i:
+            i += 1
+        else:
+            arr[i], arr[o_idx] = arr[o_idx], arr[i]
+            count += 1
+    return count
+
+
+def minimumSwaps_fails_using_dynamic_indexing(arr):
+    """
+    2023-03-01 08:01:58
+    This one results in a infinite loop because it's getting index position from the newly swapped value.
+    Pythonic swap with tuple only works if you save the swapping index into a variable.
+
+    [output]
+    # starting values
+    i: 0, arr: [4, 3, 1, 2], count: 0
+    # arr[arr[i] - 1 -> 3](2) is copied to index 0, but arr[i](4) is copied to the new arr[i] - 1(1).
+    i: 0, arr: [2, 4, 1, 2], count: 0
+    # arr[arr[i] - 1 -> 1](4) is copied to index 0, and arr[i](2) is copied to the new arr[i] - 1(3).
+    i: 0, arr: [4, 4, 1, 2], count: 1
+    # arr[arr[i] - 1 -> 3](2) is copied to index 0, and arr[i](4) is copied to the new arr[i] - 1(1).
+    i: 0, arr: [2, 4, 1, 2], count: 2
+    i: 0, arr: [4, 4, 1, 2], count: 3
+    ...
+
+
+    """
+    count = 0
+    i = 0
+    print(f"i: {i}, arr: {arr}, count: {count}")
+    while i < len(arr) - 1:
+        if arr[i] - 1 == i:
+            i += 1
+        else:
+            arr[i], arr[arr[i] - 1] = arr[arr[i] - 1], arr[i]
+            print(f"i: {i}, arr: {arr}, count: {count}")
+            count += 1
+
+    return count
+
+
 minimum_swaps = minimumSwaps_send_to_correct_position
