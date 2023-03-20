edd579833c08c9e7cb8e470a824e1fcd0134f26c
Sid Hayoun Lee <sidhlee@gmail.com>
2023-03-20 07:04:42 -0400

    add comment

--- challenges/dictionary_and_hashmaps/sherlock_and_anagrams/sherlock_and_anagrams.py
+++ challenges/dictionary_and_hashmaps/sherlock_and_anagrams/sherlock_and_anagrams.py
@@ -26,11 +26,13 @@ def sherlockAndAnagrams_correct_math(s):
     # brute forcing
     pairs = 0
     for length in range(1, len(s)):
+        # this could be a plain dict
         c = Counter()
         for i in range(len(s) + 1 - length):
             sub = s[i : i + length]
             sub = "".join(sorted(sub))
             c[sub] += 1
+        # n! * (n-1)! / 2
         num_pairs = [int(count * (count - 1) / 2) for count in c.values() if count > 1]
         pairs += sum(num_pairs)
     return pairs
