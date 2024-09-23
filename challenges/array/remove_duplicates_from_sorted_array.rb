446c3619efe27646a41b8634432542837797f50b
Sid Hayoun Lee <sidhlee@gmail.com>
2024-09-23 06:00:52 -0400

    ruby: remove_duplicates_from_sorted_array

--- challenges/array/remove_duplicates_from_sorted_array.rb
+++ challenges/array/remove_duplicates_from_sorted_array.rb
@@ -8,3 +8,16 @@ def remove_duplicates_v2(nums)
   end
   return k + 1
 end
+
+def remove_duplicates_unique(nums)
+  # Don't need to check for empty array because of 1 <= nums.length constraint
+  nums.uniq!
+  nums.length
+
+def remove_duplicates_manual_using_constraint(nums)
+  k = 1 # at least one element in array that is automatically unique
+  (1...nums.length).each do |i|
+    nums[k] = nums[i]
+    k += 1
+  k
+end
