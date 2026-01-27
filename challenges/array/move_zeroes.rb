4f9b08cbceab4a30c5f1e064af7e708a5482f7fb
Sid Hayoun Lee <sidhlee@gmail.com>
2026-01-27 07:53:05 -0500

    refactor move_zeroes_with_each method for clarity and consistency; rename and improve inline block usage

--- challenges/array/move_zeroes.rb
+++ challenges/array/move_zeroes.rb
@@ -14,7 +14,21 @@ def move_zeroes_first_attempt(nums)
   nums
 end
 
-def move_zeros_use_ranged_each(nums)
+def move_zeroes_with_each(nums)
+  # 2026-01-27 07:52:27
+    i = 0
+    nums.each do |num|
+        if num != 0
+            nums[i] = num
+            i += 1
+        end
+    end
+    (i...nums.length).each do |j|
+        nums[j] = 0
+    end
+end
+
+def move_zeros_use_ranged_each_inline_block(nums)
   insert_at = 0
   nums.each do |num|
     if num != 0
@@ -22,6 +36,7 @@ def move_zeros_use_ranged_each(nums)
       insert_at += 1
     end
   end
+  # You can use ranged each with inline block
   (insert_at...nums.length).each { |i| nums[i] = 0 }
   nums
 end
